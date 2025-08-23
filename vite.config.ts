662029a13a0651bbaab4cfaa2e747eec33392cd4
Naitik <mymailofficial1886@gmail.com>
2025-08-23 20:16:41 +0530

    qwlf

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,20 @@
 import path from "path"
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import tailwindcss from "@tailwindcss/vite"
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  css: {
+    transformer: "postcss", // 👈 force PostCSS, skip lightningcss
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
   build: {
-    outDir: 'dist',
+    outDir: "dist",
     emptyOutDir: true,
   },
 })
