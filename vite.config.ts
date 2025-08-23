6b94e360be535344cbb4cafde32a7f138f2fe6d2
Naitik <mymailofficial1886@gmail.com>
2025-08-23 21:03:33 +0530

    fet

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
 import path from "path"
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  css: {
-    transformer: "postcss", // 👈 disable lightningcss, fallback to PostCSS
-     lightningcss: false,
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-      
     },
-  },
-  build: {
-    outDir: "dist",
-    emptyOutDir: true,
   },
 })
